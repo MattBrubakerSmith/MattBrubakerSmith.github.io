e81e8e635fda6c40734ff4c94366fe8625214dfa
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-08-10 12:09:44 -0500

    Set up routing and created template files.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const Express = require('express');
-const Http = require('http');
-const Path = require('path');
+let express = require('express');
+let http = require('http');
+let path = require('path');
 
-var app = Express();
+var app = express();
+app.use(express.static('public'));
 
-app.set("views", Path.resolve(__dirname, "views"));
+let viewDir = path.resolve(__dirname, "views");
+app.set("views", viewDir);
 app.set("view engine", "ejs");
 
-const siteInfo = {
-    title: "Matthew Brubaker Smith - Web Developer • Graphic Designer",
-    author: "Matthew Brubaker Smith"
-}
-
 app.get("/", function(req, res) {
-    res.render("index", siteInfo);
+    res.render("index", {
+        page: "about"
+    });
 });
 
-Http.createServer(app).listen(8080);
+app.get("/:page", function(req, res) {
+    let page = req.params.page.toLowerCase();
+
+    res.render("index", {
+        page: page
+    });
+});
+
+http.createServer(app).listen(8080);
