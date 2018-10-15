19a37d7b658e1a2260c0273a20a1ff4aa3a900ed
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-10-15 11:53:53 -0500

    Added Assignment 2.2 and Updated 1.4. I also created a basic modal to allow users to better view diagrams.

---
+++ js/modal.js
@@ -0,0 +1,50 @@
+var Modal = (function() {
+    let instance;
+
+    function createModal() {
+        let modal = document.getElementById("modal");
+        let toggled = false;
+
+        function toggle(el) {
+            modal.innerHTML = "";
+
+            if(toggled) {
+                closeModal();
+            }
+            else {
+                let header = document.createElement("h3");
+                header.innerHTML = el.getAttribute("alt");
+                header.classList.add("yellow-text");
+                modal.appendChild(header);
+                modal.appendChild(el.cloneNode());
+                modal.classList.add("show-modal");
+                toggled = true;
+                document.querySelector("body").style.overflowY = "hidden";
+            }
+        }
+
+        function closeModal() {
+            modal.classList.remove("show-modal");
+            document.querySelector("body").style.overflowY = "auto";
+            toggled = false;
+        }
+
+        return {
+            toggle: toggle,
+            close: closeModal
+        }
+    }
+
+    function getModal() {
+        if(!instance) {
+            instance = createModal();
+        }
+        return instance;
+    }
+
+    return getModal();
+})();
+
+function toggleModal(e) {
+    Modal.toggle(e.target);
+}
